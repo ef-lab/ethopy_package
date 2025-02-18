6b901d86c63d4927cf19f84f532608efc1e5da8c
Alexandros Evangelou <alexandrose95@gmail.com>
2025-02-18 17:39:37 +0200

    Update __init__.py

--- src/ethopy/__init__.py
+++ src/ethopy/__init__.py
@@ -15,14 +15,11 @@ local_conf = ConfigurationManager()
 
 # set the datajoint parameters
 dj.config.update(local_conf.get("dj_local_conf"))
-dj.logger.setLevel(local_conf.get("dj_local_conf")['datajoint.loglevel'])
+dj.logger.setLevel(local_conf.get("dj_local_conf")["datajoint.loglevel"])
 # Schema mappings
 SCHEMATA = local_conf.get("SCHEMATA")
 
 # Initialize plugins
 plugin_manager = PluginManager(local_conf.get("plugin_path"))
 
-__all__ = ["local_conf",
-           "plugin_manager",
-           "__version__",
-           "SCHEMATA"]
+__all__ = ["local_conf", "plugin_manager", "__version__", "SCHEMATA"]
