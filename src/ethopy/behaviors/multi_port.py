9fc0eaa6741881a0120a257d1b0dc832162dc5fb
Alexandros Evangelou <alexandrose95@gmail.com>
2025-02-18 17:40:38 +0200

    Update multi_port.py

--- src/ethopy/behaviors/multi_port.py
+++ src/ethopy/behaviors/multi_port.py
@@ -31,9 +31,9 @@ class MultiPort(Behavior, dj.Manual):
 
     def __init__(self):
         super().__init__()
-        self.cond_tables = ['MultiPort', 'MultiPort.Response', 'MultiPort.Reward']
-        self.required_fields = ['response_port', 'reward_port', 'reward_amount']
-        self.default_key = {'reward_type': 'water'}
+        self.cond_tables = ["MultiPort", "MultiPort.Response", "MultiPort.Reward"]
+        self.required_fields = ["response_port", "reward_port", "reward_amount"]
+        self.default_key = {"reward_type": "water"}
 
     def is_ready(self, duration, since=False):
         position, ready_time, tmst = self.interface.in_position()
@@ -51,7 +51,7 @@ class MultiPort(Behavior, dj.Manual):
             return (ready_time + tmst - since) > duration
 
     def is_correct(self):
-        """check if the response port is correct
+        """Check if the response port is correct.
 
         if current response port is -1, then any response port is correct
         otherwise if the response port is equal to the current response port/ports,
@@ -59,6 +59,7 @@ class MultiPort(Behavior, dj.Manual):
 
         Returns:
             bool: True if correct, False otherwise
+
         """
         return self.curr_cond['response_port'] == -1 or \
             np.any(np.equal(self.response.port, self.curr_cond['response_port']))
@@ -67,7 +68,7 @@ class MultiPort(Behavior, dj.Manual):
         return self.interface.off_proximity()
 
     def reward(self, tmst=0):
-        """give reward at latest licked port
+        """Give reward at latest licked port.
 
         After the animal has made a correct response, give the reward at the
         first port that animal has licked and is definded as reward.
@@ -77,13 +78,14 @@ class MultiPort(Behavior, dj.Manual):
 
         Returns:
             bool: True if rewarded, False otherwise
+
         """
         # if response and reward ports are the same no need of tmst
         if self.response.reward:
             tmst = 0
         # check that the last licked port is also a reward port
         licked_port = self.is_licking(since=tmst, reward=True)
-        if licked_port == self.curr_cond['reward_port']:
+        if licked_port == self.curr_cond["reward_port"]:
             self.interface.give_liquid(licked_port)
             self.log_reward(self.reward_amount[self.licked_port])
             self.update_history(self.response.port, self.reward_amount[self.licked_port])
