5ec0dd7d64ea227a84b88d2fc43ec2d01613f605
Alexandros Evangelou <alexandrose95@gmail.com>
2025-03-13 14:56:24 +0200

    remove space

--- src/ethopy/experiments/match_port.py
+++ src/ethopy/experiments/match_port.py
@@ -56,7 +56,7 @@ class Experiment(State, ExperimentClass):
         "punish_duration": 1000,
         "abort_duration": 0,
         "noresponse_intertrial": True,
-        "incremental_punishment" : 0,
+        "incremental_punishment": 0,
         **ExperimentClass.Block().dict(),
     }
 
