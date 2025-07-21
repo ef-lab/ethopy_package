d1a81edd189e9412ae7314b02325a5fc15f895f3
Alexandros Evangelou <alexandrose95@gmail.com>
2025-07-21 11:14:35 +0300

    Reformat conditions dict for readability

--- src/ethopy/task/free_water.py
+++ src/ethopy/task/free_water.py
@@ -16,19 +16,17 @@ exp = Experiment()
 exp.setup(logger, MultiPort, session_params)
 block = exp.Block(trial_selection='biased')
 
-conditions = exp.make_conditions(stim_class=Stimulus(), 
-                                 conditions={**block.__dict__,                            
+conditions = exp.make_conditions(stim_class=Stimulus(),
+                                 conditions={**block.__dict__,                   
                                              'timeout_duration'   : 0,
                                              'intertrial_duration': 500,
                                              'init_duration'      : 0,
                                              'delay_duration'     : 0,
                                              'reward_amount'      : 10,
-                                             'reward_port': -1,
-                                             'response_port': -1,
+                                             'reward_port'        : -1,
+                                             'response_port'      : -1,
                                              })
 
 # run experiments
 exp.push_conditions(conditions)
 exp.start()
-
-
